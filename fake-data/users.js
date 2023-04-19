2983302fc79f53bc2cbd309c398b2feaa2e75e18
Inae Choi <inaechoi89@gmail.com>
2023-04-19 20:26:34 +0900

    Feat: fake-data 생성

---
+++ fake-data/users.js
@@ -0,0 +1,36 @@
+const users = [
+  {
+    userId: "snowlover@gmail.com",
+    password: "snow123",
+    userName: "snowlover",
+    subscribed: [{ id: 8, price: 7900 }],
+    like: [
+      { id: 507129, type: "movie", modified_at: "2021-12-25T12:59:32.746Z" },
+      { id: 37692, type: "tv", modified_at: "2020-12-31T12:59:32.746Z" },
+    ],
+    comingUp: [
+      { id: 18465, type: "movie", modified_at: "2021-05-18T12:59:32.746Z" },
+    ],
+    history: [
+      { id: 829, type: "movie", modified_at: "2023-04-15T12:59:32.746Z" },
+      { id: 54823, type: "tv", modified_at: "2022-01-01T12:59:32.746Z" },
+    ],
+  },
+  {
+    userId: "squid@gmail.com",
+    password: "squid456",
+    userName: "squid",
+    subscribed: [{ id: 8, price: 7900 }],
+    like: [
+      { id: 507129, type: "movie", modified_at: "2021-12-25T12:59:32.746Z" },
+      { id: 37692, type: "tv", modified_at: "2020-12-31T12:59:32.746Z" },
+    ],
+    comingUp: [
+      { id: 18465, type: "movie", modified_at: "2021-05-18T12:59:32.746Z" },
+    ],
+    history: [
+      { id: 829, type: "movie", modified_at: "2023-04-15T12:59:32.746Z" },
+      { id: 54823, type: "tv", modified_at: "2022-01-01T12:59:32.746Z" },
+    ],
+  },
+];
