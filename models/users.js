4bf4a0d525b1cb7dbf69d69618c205bcc7531f5f
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-14 23:26:10 +0900

    Fix: 오타 수정

--- models/users.js
+++ models/users.js
@@ -36,7 +36,7 @@ let users = [
       { id: 736526, type: 'movie', modified_at: '2022-07-21T12:59:32.746Z' },
       { id: 675353, type: 'movie', modified_at: '2022-04-01T12:59:32.746Z' },
       { id: 82856, type: 'tv', modified_at: '2022-12-04T12:59:32.746Z' },
-      { id: 60574, type: 'tv', modified_at: '2022-11-016T12:59:32.746Z' },
+      { id: 60574, type: 'tv', modified_at: '2022-11-16T12:59:32.746Z' },
     ],
   },
   {
