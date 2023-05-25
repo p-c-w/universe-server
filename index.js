08be123286affc2e2ddfeeca591ab73709b14c6b
5kdk <tylerk1121@gmail.com>
2023-05-25 19:02:26 +0900

    Feat: CORS URL 추가

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ require('dotenv').config();
 const jwtMiddleware = require('./lib/authChecker');
 
 const app = express();
-const PORT = process.env.PORT || 9990;
+const { PORT } = process.env;
 
 const auth = require('./routes/api/auth');
 const users = require('./routes/api/users');
 
+const corsOptions = {
+  origin: process.env.CORS_CLIENT_URL,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(jwtMiddleware);
@@ -18,6 +25,4 @@ app.use(jwtMiddleware);
 app.use('/api/auth', auth);
 app.use('/api/users', users);
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
+app.listen(PORT);
